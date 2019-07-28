d76e1de58135272e49d3a010119489d06d868b66
Sam Velasquez <atrox.lapa@hotmail.com>
2019-07-27 23:41:34 -0300

    feat: add search

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const readline = require('readline-sync')
+
 function start() {
 
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
-
+    content.prefix = askAndReturnPrefix()
     function askAndReturnSearchTerm() {
-        return 'TERM EXAMPLE'
+        return readline.question("Type a Wikipedia search term:")
+    }
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Select an Option', )
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        
+        return selectedPrefixText
     }
     console.log(content)
 }

e4f15fe5a3e7fb037a76e68a6b8503087aa6f6b4
Sam Velasquez <atrox.lapa@hotmail.com>
2019-07-27 23:23:43 -0300

    feat : add package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+function start() {
+
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+
+    function askAndReturnSearchTerm() {
+        return 'TERM EXAMPLE'
+    }
+    console.log(content)
+}
+
+start()
